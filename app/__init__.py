c2e0a608bd98bb86c6a3b9f2d57d201659dbbb22
Muhammad Ammad <ammadmughal567@gmail.com>
2024-02-08 20:08:19 +0500

    Added pages and API calling functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect, url_for
 from flask_socketio import SocketIO, emit
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import json
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,14 +14,48 @@ if __name__ == '__main__':
 users = {}
 
 @app.route('/')
-def index():
-    return render_template('common/index.html')
+def index(name='', phone=''):
+    name = name
+    phone=phone
+    return render_template('common/landing.html', name=name, phone=phone)
 
 
 @app.route('/about')
 def about():
     return render_template('common/about.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login(): #Method to call a formdata API
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        url = "http://117.20.28.178:8038/customerFaciliationPortal/signIn"
+
+        payload = {
+            'userId': username,
+            'password': password
+        }
+
+        # Encode payload as multipart form-data
+        multipart_data = MultipartEncoder(fields=payload)
+
+        headers = {
+            'Content-Type': multipart_data.content_type
+        }
+
+        response = requests.post(url, headers=headers, data=multipart_data)
+        response_json = response.json()
+        if(response_json['status'] == '200'):
+            name =  response_json['name']
+            phone =  response_json['phoneNo']
+            return redirect(url_for('index', name=name, phone=phone))
+        else:
+            message = response_json['message']
+            return render_template('authentication/login.html', message=message)
+        
+    elif request.method == 'GET':
+        return render_template('authentication/login.html')
 
 @app.route('/chat')
 def chat():
